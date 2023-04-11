c6b4eb4bcbbdaf21c630e02008069497af4dc6c3
shuozhi <shuozhi.zp@wayxtech.com>
2023-04-11 22:33:14 +0800

    添加注释

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import {ApiPromise, WsProvider} from '@polkadot/api';
-
+/// substrate地址
 const SOCKET_URL = 'ws://127.0.0.1:9944';
-
+/// 连接到substrate
 const connectSubstrate = async () => {
     const wsProvider = new WsProvider(SOCKET_URL);
     const api = await ApiPromise.create({provider: wsProvider});
@@ -9,14 +9,17 @@ const connectSubstrate = async () => {
     console.log('Connection to Substrate is OK.')
     return api;
 }
+/// 订阅event
 const subscribeEvents = async (api: ApiPromise) => {
     await api.query.system.events((events: any) => {
+        /// 打印event
         console.log(`@ [ ${events.length} ] events received`)
         events.forEach((event: any, index: number) => {
             console.log(`-->[ ${index} ]: ${event}`)
         })
     });
 }
+/// 主函数
 const main = async () => {
     const api = await connectSubstrate();
     await subscribeEvents(api);
